e2573a1de4826b73f492f587a798f82c45d5357b
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2025-04-19 14:00:45 +0700

    feat(layout-admin): setting layout admin

---
+++ resources/views/admin/layout/main.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="id">
+
+@include('admin.layouts.head')
+
+<div class="page-wrapper">
+
+    <!-- Page Header Start-->
+    @include('admin.layouts.setting-head')
+    <!-- Page Header Ends-->
+
+    <!-- Page Body Start-->
+    <div class="page-body-wrapper">
+
+        <!-- Page Sidebar Start-->
+        @include('admin.layouts.sidebar')
+        <!-- Page Sidebar Ends-->
+
+        <!-- Right sidebar Start-->
+        @include('admin.layouts.right-sidebar')
+        <!-- Right sidebar Ends-->
+
+        <div class="page-body">
+
+            {{-- content --}}
+            @yield('content')
+            {{-- end content --}}
+
+        </div>
+
+        <!-- footer start-->
+        @include('admin.layouts.footer')
+        <!-- footer end-->
+    </div>
+</div>
+
+@stack('scripts')
+
+<!-- latest jquery-->
+<script src="{{ asset('assets/admin/js/jquery-3.3.1.min.js') }}"></script>
+
+<!-- Bootstrap js-->
+<script src="{{ asset('assets/admin/js/bootstrap.bundle.min.js') }}"></script>
+
+<!-- feather icon js-->
+<script src="{{ asset('assets/admin/js/icons/feather-icon/feather.min.js') }}"></script>
+<script src="{{ asset('assets/admin/js/icons/feather-icon/feather-icon.js') }}"></script>
+
+<!-- Sidebar jquery-->
+<script src="{{ asset('assets/admin/js/sidebar-menu.js') }}"></script>
+
+<!--chartist js-->
+<script src="{{ asset('assets/admin/js/chart/chartist/chartist.js') }}"></script>
+
+<!--chartjs js-->
+<script src="{{ asset('assets/admin/js/chart/chartjs/chart.min.js') }}"></script>
+
+<!-- lazyload js-->
+<script src="{{ asset('assets/admin/js/lazysizes.min.js') }}"></script>
+
+<!--copycode js-->
+<script src="{{ asset('assets/admin/js/prism/prism.min.js') }}"></script>
+<script src="{{ asset('assets/admin/js/clipboard/clipboard.min.js') }}"></script>
+<script src="{{ asset('assets/admin/js/custom-card/custom-card.js') }}"></script>
+
+<!--counter js-->
+<script src="{{ asset('assets/admin/js/counter/jquery.waypoints.min.js') }}"></script>
+<script src="{{ asset('assets/admin/js/counter/jquery.counterup.min.js') }}"></script>
+<script src="{{ asset('assets/admin/js/counter/counter-custom.js') }}"></script>
+
+<!--peity chart js-->
+<script src="{{ asset('assets/admin/js/chart/peity-chart/peity.jquery.js') }}"></script>
+
+<!-- Apex Chart Js -->
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
+<!--sparkline chart js-->
+<script src="{{ asset('assets/admin/js/chart/sparkline/sparkline.js') }}"></script>
+
+<!--Customizer admin-->
+<script src="{{ asset('assets/admin/js/admin-customizer.js') }}"></script>
+
+<!--dashboard custom js-->
+<script src="{{ asset('assets/admin/js/dashboard/default.js') }}"></script>
+
+<!--right sidebar js-->
+<script src="{{ asset('assets/admin/js/chat-menu.js') }}"></script>
+
+<!--height equal js-->
+<script src="{{ asset('assets/admin/js/height-equal.js') }}"></script>
+
+<!-- lazyload js-->
+<script src="{{ asset('assets/admin/js/lazysizes.min.js') }}"></script>
+
+<!--script admin-->
+<script src="{{ asset('assets/admin/js/admin-script.js') }}"></script>
+
+<!-- Datatables js-->
+<script src="{{ asset('assets/admin/js/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('assets/admin/js/datatables/custom-basic.js') }}"></script>
+
+</html>
---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,119 @@
+<!-- Page Sidebar Start-->
+<div class="page-sidebar">
+    <div class="main-header-left d-none d-lg-block">
+        <div class="logo-wrapper">
+            <a href="index.html">
+                <img class="d-none d-lg-block blur-up lazyloaded"
+                    src="{{ asset('assets/admin/images/dashboard/multikart-logo.png') }}" alt="">
+            </a>
+        </div>
+    </div>
+    <div class="sidebar custom-scrollbar">
+        <a href="javascript:void(0)" class="sidebar-back d-lg-none d-block"><i class="fa fa-times"
+                aria-hidden="true"></i></a>
+        <div class="sidebar-user">
+            <img class="img-60" src="{{ asset('assets/admin/images/dashboard/user3.jpg') }}" alt="#">
+            <div>
+                <h6 class="f-14">JOHN</h6>
+                <p>general manager.</p>
+            </div>
+        </div>
+        <ul class="sidebar-menu">
+            <li>
+                <a class="sidebar-header" href="{{ route('admin.index') }}">
+                    <i data-feather="home"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <li>
+                <a class="sidebar-header" href="javascript:void(0)">
+                    <i data-feather="dollar-sign"></i>
+                    <span>Event</span>
+                    <i class="fa fa-angle-right pull-right"></i>
+                </a>
+                <ul class="sidebar-submenu">
+                    <li>
+                        <a href="{{ route('admin.event') }}">
+                            <i class="fa fa-circle"></i>Event List
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <a class="sidebar-header" href="javascript:void(0)">
+                    <i data-feather="tag"></i>
+                    <span>Members</span>
+                    <i class="fa fa-angle-right pull-right"></i>
+                </a>
+                <ul class="sidebar-submenu">
+                    <li>
+                        <a href="{{ route('admin.member') }}">
+                            <i class="fa fa-circle"></i>List Member
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="disabled">
+                <a class="sidebar-header disabled" href="media.html">
+                    <i data-feather="camera"></i>
+                    <span>Galery</span>
+                </a>
+            </li>
+
+            <li class="disabled">
+                <a class="sidebar-header disabled" href="javascript:void(0)">
+                    <i data-feather="align-left"></i>
+                    <span>Blog</span>
+                    <i class="fa fa-angle-right pull-right"></i>
+                </a>
+                <ul class="sidebar-submenu">
+                    <li>
+                        <a href="menu-list.html">
+                            <i class="fa fa-circle"></i>Menu Lists
+                        </a>
+                    </li>
+                    <li>
+                        <a href="create-menu.html">
+                            <i class="fa fa-circle"></i>Create Menu
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="disabled">
+                <a class="sidebar-header disabled" href="support-ticket.html"><i data-feather="phone"></i><span>Support
+                        Ticket</span>
+                </a>
+            </li>
+
+            <li class="disabled">
+                <a class="sidebar-header disabled" href="reports.html"><i
+                        data-feather="bar-chart"></i><span>Reports</span>
+                </a>
+            </li>
+
+            <li class="disabled">
+                <a class="sidebar-header disabled" href="javascript:void(0)"><i
+                        data-feather="settings"></i><span>Settings</span><i
+                        class="fa fa-angle-right pull-right"></i></a>
+                <ul class="sidebar-submenu">
+                    <li>
+                        <a href="profile.html"><i class="fa fa-circle"></i>Profile
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="disabled">
+                <a class="sidebar-header disabled" href="forgot-password.html">
+                    <i data-feather="key"></i>
+                    <span>Forgot Password</span>
+                </a>
+            </li>
+
+        </ul>
+    </div>
+</div>
+<!-- Page Sidebar Ends-->
