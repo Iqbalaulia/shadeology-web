daf269d14bdfa7816f9835147ea10d4313f40db3
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2025-04-26 20:18:49 +0700

    fix(banner): adjusment shadeology banner section

---
+++ resources/views/client/components/banner/banner.blade.php
@@ -0,0 +1,34 @@
+<section class="banner-style1-home5 home7-style">
+    <div class="leaf-1 d-none d-lg-block bounce-y banner-bg"><img
+            src="{{ asset('assets/client/images/banner/home5-banner3.png') }}" alt=""></div>
+    <div class="banner-bottom"><img src="{{ asset('assets/client/images/banner/home5-banner-bottom.png') }}"
+            alt=""></div>
+    <div class="auto-container">
+        <div class="row">
+            <div class="col-xl-6 d-none d-xl-block">
+                <div class="image-content">
+                    <div class="bg-circle zoom-one"></div>
+                    <img class="img" src="{{ asset('assets/client/images/banner/banner-shadeology.png') }}"
+                        alt="">
+                    <img class="img-2 bounce-x" src="{{ asset('assets/client/images/banner/home5-banner2.png') }}"
+                        alt="">
+                </div>
+            </div>
+            <div class="col-xl-5">
+                <div class="content-column">
+                    <div class="inner-content">
+                        <div class="title-bg"><img src="{{ asset('assets/client/images/icons/icon2.png') }}"
+                                alt=""><span class="bg-text">Shadeology</span></div>
+                        <h2 class="title">Discover Your Best Colors & Elevate Your Everyday Look</h2>
+                        <p class="text">Shadeology is your trusted partner for discovering your perfect colors and
+                            enhancing your natural beauty.
+                            We specialize in personal color analysis, makeup consulting, and beauty coaching, helping
+                            you find the shades.
+                        </p>
+                        <a href="page-contact.html" class="theme-btn btn-style-transparent mt-25">Appointment Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/client/pages/home/index.blade.php
+++ resources/views/client/pages/home/index.blade.php
@@ -1,37 +1,7 @@
 @extends('client.layout.main')
 @section('content')
     <!-- Banner Start -->
-    <section class="banner-style1-home5 home7-style">
-        <div class="leaf-1 d-none d-lg-block bounce-y"><img src="{{ asset('assets/client/images/banner/home5-banner3.png') }}"
-                alt=""></div>
-        <div class="banner-bottom"><img src="{{ asset('assets/client/images/banner/home5-banner-bottom.png') }}"
-                alt=""></div>
-        <div class="auto-container">
-            <div class="row">
-                <div class="col-xl-6 d-none d-xl-block">
-                    <div class="image-content">
-                        <div class="bg-circle zoom-one"></div>
-                        <img class="img" src="{{ asset('assets/client/images/banner/home7-banner1.png') }}"
-                            alt="">
-                        <img class="img-2 bounce-x" src="{{ asset('assets/client/images/banner/home5-banner2.png') }}"
-                            alt="">
-                    </div>
-                </div>
-                <div class="col-xl-5">
-                    <div class="content-column">
-                        <div class="inner-content">
-                            <div class="title-bg"><img src="{{ asset('assets/client/images/icons/icon2.png') }}"
-                                    alt=""><span class="bg-text">Purerelax</span></div>
-                            <h2 class="title">Best Spa & <br /> Beauty care</h2>
-                            <p class="text">Embrace your beauty at PureRelax, where we have meticulously crafted a
-                                paradise of splendor for your ultimate comfort and enjoyment.</p>
-                            <a href="page-contact.html" class="theme-btn btn-style-transparent mt-25">Appointment Now</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('client.components.banner.banner')
     <!-- End Banner End -->
 
     <!-- Service Section -->
