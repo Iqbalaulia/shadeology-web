3eed1c17903ecdeb47452cbe861c7d8454a09722
Muhammad Iqbal A.R <miqbalar12@gmail.com>
2025-05-03 10:44:29 +0700

    fix:(deployment): deployment vercel project

--- resources/views/client/layout/setting-head.blade.php
+++ resources/views/client/layout/setting-head.blade.php
@@ -5,6 +5,7 @@
     <link href="{{ asset('assets/client/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/client/css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/client/css/product-recommendation.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/client/css/responsive-style.css') }}" rel="stylesheet">
 
     <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
     <link rel="icon" href="images/favicon.png" type="image/x-icon">
@@ -12,5 +13,9 @@
     <!-- Responsive -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+    {{-- Setting vercel --}}
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    {{-- Setting vercel --}}
 
 </head>
